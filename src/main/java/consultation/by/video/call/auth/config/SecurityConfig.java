ae035d5efad3b6bedf72dbe0f51ae452dfef09fc
Tomas Padilla <padillatomasagustin@gmail.com>
2022-04-05 09:25:41 +0200

    ServerSessionCookie

--- src/main/java/consultation/by/video/call/auth/config/SecurityConfig.java
+++ src/main/java/consultation/by/video/call/auth/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -43,6 +44,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         firewall.setAllowSemicolon(true);
         firewall.setAllowBackSlash(true);
         return firewall;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
+
     }
 
     @Autowired
